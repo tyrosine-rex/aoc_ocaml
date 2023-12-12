ae9336729fdc8c6a86389a376c22c8b3f77722b9
thomas lahaie <thomas.lahaie@icloud.com>
2023-12-13 00:41:11 +0100

    Day 1 pt1&2 done!

--- bin/main.ml
+++ bin/main.ml
@@ -1,5 +1,3 @@
-let () = 
-  let res1 = Lib.Day1.pt1 in 
-  print_endline res1;
-  (* let res2 = Lib.Day2.pt2 in 
-  print_endline res2 *)
+let () =
+  print_endline Lib.Day1.results;
+
--- lib/day1.ml
+++ lib/day1.ml
@@ -1,1 +1,38 @@
-let pt1 = ""
+let rgx1 = "(\\d)(?:.*(\\d))?"
+let rgx2 = "(\\d|zero|one|two|three|four|five|six|seven|eight|nine)(?:.*(\\d|zero|one|two|three|four|five|six|seven|eight|nine))?"
+
+let read_lines file =
+  let ic = In_channel.open_text file in
+  In_channel.input_lines ic
+
+let digit_of_litteral l =
+  match l with
+    | "zero"  | "0" -> "0"
+    | "one"   | "1" -> "1"
+    | "two"   | "2" -> "2"
+    | "three" | "3" -> "3"
+    | "four"  | "4" -> "4"
+    | "five"  | "5" -> "5"
+    | "six"   | "6" -> "6"
+    | "seven" | "7" -> "7"
+    | "eight" | "8" -> "8"
+    | "nine"  | "9" -> "9"
+    | _ -> ""
+
+let parse_line rgx line = match Re.exec_opt rgx line with
+  | None -> 0
+  | Some m -> match Re.Group.get_opt m 1, Re.Group.get_opt m 2 with
+                | None, _ -> 0
+                | Some g, None -> int_of_string (digit_of_litteral g ^ digit_of_litteral g)
+                | Some g1, Some g2 -> int_of_string (digit_of_litteral g1 ^ digit_of_litteral g2)
+
+let sum_lines rgx lines =
+  let rec aux res = function
+    | [] -> res
+    | hd::tl -> aux (res + parse_line rgx hd) tl
+  in aux 0 lines
+
+let results =
+  let r1 = sum_lines (Re.Perl.compile_pat rgx1) (read_lines "./input/day1.txt") in
+  let r2 = sum_lines (Re.Perl.compile_pat rgx2) (read_lines "./input/day1.txt") in
+  Printf.sprintf "pt1: %d\tpt2: %d\n" r1 r2
