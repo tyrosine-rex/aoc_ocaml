c142a950e653c24106ab915217e026c2d5b6e717
thomas lahaie <thomas.lahaie@icloud.com>
2023-12-15 01:44:40 +0100

    day2-2 done

--- lib/day2.ml
+++ lib/day2.ml
@@ -5,27 +5,26 @@ type game = {
   sets: (int * color) list list;
 }
 
-
 let rgx_id = Re.Perl.compile_pat "Game (\\d+):"
 let rgx_tok = Re.Perl.compile_pat "(\\d+) (green|red|blue)"
 
-let color_of_string s = match s with 
-  | "red" -> Red 
+let color_of_string s = match s with
+  | "red" -> Red
   | "green" -> Green
   | "blue" -> Blue
   | _ -> failwith "mauvaise couleur"
 
-let tokenize set_str = 
-  let toks = Re.matches rgx_tok set_str in 
-  let convert tk = 
+let tokenize set_str =
+  let toks = Re.matches rgx_tok set_str in
+  let convert tk =
     int_of_string (List.nth (Common.split_with " " tk) 0),
     color_of_string (List.nth (Common.split_with " " tk) 1)
   in List.map convert toks
 
-let parse_sets line = 
-  let sets = Common.split_with  "; |Game \\d+: " line in  
-  let rec aux res = function 
-    | [] -> res 
+let parse_sets line =
+  let sets = Common.split_with  "; |Game \\d+: " line in
+  let rec aux res = function
+    | [] -> res
     | hd::tl -> aux (tokenize hd::res) tl
 in aux [] sets
 
@@ -35,27 +34,45 @@ let parse_line line = {
 }
 
 let is_possible_game r g b game =
-  let is_possible_sets sets = 
-    let rec aux = function 
+  let is_possible_sets sets =
+    let rec aux = function
       | [] -> true
       | (n, c)::tl when c = Red && n <= r     -> aux tl
       | (n, c)::tl when c = Green && n <= g   -> aux tl
-      | (n, c)::tl when c = Blue && n <= b   -> aux tl 
+      | (n, c)::tl when c = Blue && n <= b   -> aux tl
       | _ -> false
-    in aux sets 
-  in let rec aux2 = function 
-    | [] -> true 
+    in aux sets
+  in let rec aux2 = function
+    | [] -> true
     | hd::tl -> if is_possible_sets hd then aux2 tl else false
 in aux2 game.sets
 
-
-let rec sum_id = function  
-  | [] -> 0 
+let rec sum_id = function
+  | [] -> 0
   | hd::tl -> hd.id + sum_id tl
 
-let results = 
-  let lines = Common.read_lines "./input/day2.txt" in 
-  let games = List.map parse_line lines in 
-  let filtered_games = List.filter (is_possible_game 12 13 14) games in 
-  let r1 = sum_id filtered_games in 
-  Printf.sprintf "day2\tpt1: %d\tpt2: \n" r1
+let rec max_of_l r color = function
+  | (n, c)::tl when c = color -> if n > r then max_of_l n color tl else max_of_l r color tl
+  | [] -> r
+  | _::tl -> max_of_l r color tl
+
+let calc_power game =
+  let flat_set = List.flatten game.sets in
+  let max_red = max_of_l 0 Red flat_set in
+  let max_green = max_of_l 0 Green flat_set in
+  let max_blue = max_of_l 0 Blue flat_set in
+  max_red * max_green * max_blue
+
+let rec sum = function
+  | [] -> 0
+  | hd::tl -> hd+(sum tl)
+
+let results =
+  let lines = Common.read_lines "./input/day2.txt" in
+  let games = List.map parse_line lines in
+  let filtered_games = List.filter (is_possible_game 12 13 14) games in
+  let r1 = sum_id filtered_games in
+  let powers = List.map calc_power games in
+  let r2 = sum powers in
+  Printf.sprintf "day2\tpt1: %d\tpt2: %d\n" r1 r2
+
