ea54186cad8047e60d94e5e552f359f2af827bad
tyrosine-rex <114239483+tyrosine-rex@users.noreply.github.com>
2023-12-16 20:22:10 +0100

    Update main.ml

--- src/main.ml
+++ src/main.ml
@@ -2,7 +2,7 @@ let () =
   print_endline (Lib.Day1.results "./input/day1.txt");
   print_endline (Lib.Day2.results "./input/day2.txt");
   
-  (* print_endline (Lib.Day4.results "./input/day4.txt"); very slow --> need memoisation, multiple same traversal ! *)
+  (* print_endline (Lib.Day4.results "./input/day4.txt"); part 2 is very slow (some seconds) --> need memoisation, multiple same traversal ! *)
   
   print_endline (Lib.Day6.results "./input/day6.txt");
   print_endline (Lib.Day9.results "./input/day9.txt");
