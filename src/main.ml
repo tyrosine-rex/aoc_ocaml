c6ae604a04b1cd4aa8fba2997f214d6a173a1dec
thomas lahaie <thomas.lahaie@icloud.com>
2023-12-18 11:57:29 +0100

    day 7 pt1

--- src/main.ml
+++ src/main.ml
@@ -3,6 +3,9 @@ let () =
   print_endline (Lib.Day2.results "./input/day2.txt");
   
   (* print_endline (Lib.Day4.results "./input/day4.txt"); part 2 is very slow (some seconds) --> need memoisation, multiple same traversal ! *)
-  
+  print_endline "day4\tSKIPPED --> WORK BUT SLOW";
+
   print_endline (Lib.Day6.results "./input/day6.txt");
+  print_endline (Lib.Day7.results "./input/day7.txt");
+  (* print_endline (Lib.Day7pt2.results "./input/day7.txt"); *)
   print_endline (Lib.Day9.results "./input/day9.txt");
